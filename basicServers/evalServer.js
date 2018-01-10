e0d7372b3d861168397f8ae8c88ca31c12310cc3
student <student@dc-19.mobile.asu.edu>
2018-01-10 09:15:31 -0700

    basicServers

---
+++ basicServers/evalServer.js
@@ -0,0 +1,15 @@
+var http = require("http");
+
+var callback = function (req, res) { // req -> request object; res -> response object
+  var eq = req.url.replace("/","");;
+
+  if(eq.indexOf("favicon") >=0 ) return;
+
+  var r = eq + " = " + eval(eq) + "\n";
+   res.writeHead(200, {'Content-Type': 'text/plain'}); // send response header
+   res.end(r); // send response body
+}
+
+var server = http.createServer(callback) // create an http server
+server.listen(1234, "127.0.0.1"); // make server listen to port 1234
+console.log("Server running at: "+ "http://127.0.0.1:1234");
---
+++ basicServers/staticServer.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
+var methodOverride = require('method-override');
+var hostname = process.env.HOSTNAME || 'localhost';
+var port = 8080;
+
+app.get("/", function (req, res) {
+    console.log("GET req arrived")
+        res.send("hello world")
+});
+
+app.use(methodOverride());
+app.use(bodyParser());
+app.use(express.static(__dirname + '/public'));
+app.use(errorHandler());
+
+console.log("Simple static server listening at http://" + hostname + ":" + port);
+app.listen(port);
