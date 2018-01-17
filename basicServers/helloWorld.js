78720cf4ec87479d0a5a28f58bb8ffc8e773d414
student <student@dc-26.mobile.asu.edu>
2018-01-17 10:45:01 -0700

    ac

--- basicServers/helloWorld.js
+++ basicServers/helloWorld.js
@@ -1,9 +1,9 @@
-
 var http = require("http");
 
 var callback = function (req, res) { // req -> request object; res -> response object
    res.writeHead(200, {'Content-Type': 'text/plain'}); // send response header
-   res.end("hello world"); // send response body
+	var st = "sjdvjhsdbgkjds"
+   res.end(st); // send response body
 }
 
 var server = http.createServer(callback) // create an http server
