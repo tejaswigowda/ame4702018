fd42372e0082021bf04761b3affbecd0336449a8
Ubuntu <ubuntu@ip-10-0-0-221.us-west-1.compute.internal>
2018-01-29 17:19:20 +0000

    ac

--- basicServers/evalServer.js
+++ basicServers/evalServer.js
@@ -11,5 +11,5 @@ var callback = function (req, res) { // req -> request object; res -> response o
 }
 
 var server = http.createServer(callback) // create an http server
-server.listen(1234, "127.0.0.1"); // make server listen to port 1234
+server.listen(1234); // make server listen to port 1234
 console.log("Server running at: "+ "http://127.0.0.1:1234");
