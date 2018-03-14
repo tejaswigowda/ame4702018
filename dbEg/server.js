059ba2dbc3310cb789412961e5b439e7dcf69ca4
tejaswigowda <tejaswi@foxyninjastudios.com>
2018-03-14 10:14:46 -0700

    in class

---
+++ dbEg/server.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+var MS = require("mongoskin");
+var errorHandler = require('errorhandler');
+var methodOverride = require('method-override');
+var hostname = process.env.HOSTNAME || 'localhost';
+var port = 1234;
+
+var db = mongo.db("mongodb://localhost:27017/ame470")
+
+
+app.get("/", function (req, res) {
+    res.redirect("index.html")
+});
+
+app.get("/getData", function (req, res) {
+  db.collection('names').find().toArray(function(err, items) {
+    res.send(items.toString());
+  });
+});
+
+app.use(methodOverride());
+app.use(bodyParser());
+app.use(express.static(__dirname + '/public'));
+app.use(errorHandler());
+
+console.log("Simple static server listening at http://" + hostname + ":" + port);
+app.listen(port);
